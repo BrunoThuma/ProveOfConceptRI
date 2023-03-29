61231fb79188aa6e98ec58501e436096328a58ff
Bruno Thuma <bruno.thuma@hotmail.com>
2023-03-29 09:56:41 -0300

    Initial Commit

---
+++ ProveOfConceptRI/ProveOfConceptRIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProveOfConceptRIApp.swift
+//  ProveOfConceptRI
+//
+//  Created by Bruno Thuma on 29/03/23.
+//
+
+import SwiftUI
+
+@main
+struct ProveOfConceptRIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
