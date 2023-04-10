383d213aada5d3d14e6071230e4f5b0e6cdaecd3
Bruno Thuma <bruno.thuma@hotmail.com>
2023-04-10 11:25:17 -0300

    receive deeplink parameters calls deeplink

--- ProveOfConceptRI/ContentView.swift
+++ ProveOfConceptRI/ContentView.swift
@@ -8,19 +8,46 @@
 import SwiftUI
 
 struct ContentView: View {
+    let deepLinkURLString = "stage://deeplink?param1=value1&param2=UrlToTheDummy"
+    
+    @State var param1: String?
+    @State var param2: String?
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack {
+                if let p1 = param1, let p2 = param2 {
+                    VStack {
+                        Text("Received parameters")
+                        Text(p1)
+                        Text(p2)
+                    }
+                } else {
+                    Text("No parameters received")
+                }
+                NavigationLink(
+                    "Go to CameraView",
+                    destination: CameraView())
+                .buttonStyle(.borderedProminent)
+                Button("Open Stage") {
+                    if let deepLinkURL = URL(string: deepLinkURLString) {
+                                    UIApplication.shared.open(deepLinkURL, options: [:], completionHandler: nil)
+                                }
+                }
+            }
         }
-        .padding()
+        .onOpenURL { url in
+            if let urlComponents = URLComponents(url: url, resolvingAgainstBaseURL: false),
+               let params = urlComponents.queryItems {
+                param1 = params.first(where: { $0.name == "param1" })?.value
+                param2 = params.first(where: { $0.name == "param2" })?.value
+            }
+        }
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
+//struct ContentView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ContentView()
+//    }
+//}
