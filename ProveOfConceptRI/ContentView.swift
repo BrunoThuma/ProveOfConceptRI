e2d4c71da6cb3ecc68f4d052347ea56cb49b16df
Bruno Thuma <bruno.thuma@hotmail.com>
2023-05-23 01:29:11 -0300

    picks image from gallery and sends to stage

--- ProveOfConceptRI/ContentView.swift
+++ ProveOfConceptRI/ContentView.swift
@@ -6,17 +6,21 @@
 //
 
 import SwiftUI
+import PhotosUI
 
 struct ContentView: View {
-    let deepLinkURLString = "stage://deeplink?param1=value1&param2=UrlToTheDummy"
-    
-    @State var param1: String?
+    @State private var pickedItemFromGallery: PhotosPickerItem?
+    @State var surveyId: String?
     @State var param2: String?
     
     var body: some View {
         NavigationStack {
             VStack {
-                if let p1 = param1, let p2 = param2 {
+                PhotosPicker("Select image for",
+                             selection: $pickedItemFromGallery,
+                             matching: .images)
+                .buttonStyle(.borderedProminent)
+                if let p1 = surveyId, let p2 = param2 {
                     VStack {
                         Text("Received parameters")
                         Text(p1)
@@ -25,25 +29,111 @@ struct ContentView: View {
                 } else {
                     Text("No parameters received")
                 }
-                NavigationLink(
-                    "Go to CameraView",
-                    destination: CameraView())
-                .buttonStyle(.borderedProminent)
-                Button("Open Stage") {
-                    if let deepLinkURL = URL(string: deepLinkURLString) {
-                                    UIApplication.shared.open(deepLinkURL, options: [:], completionHandler: nil)
-                                }
-                }
+//                NavigationLink(
+//                    "Go to CameraView",
+//                    destination: CameraView())
+                
+                Button("Open Stage without image", action: callStage)
+                    .buttonStyle(.borderedProminent)
             }
         }
-        .onOpenURL { url in
-            if let urlComponents = URLComponents(url: url, resolvingAgainstBaseURL: false),
-               let params = urlComponents.queryItems {
-                param1 = params.first(where: { $0.name == "param1" })?.value
-                param2 = params.first(where: { $0.name == "param2" })?.value
+        .onOpenURL(perform: handleDeepLinkCall(using:))
+        .onChange(of: pickedItemFromGallery, perform: loucuradaDasImages(_:))
+    }
+    
+    private func loucuradaDasImages(_ : any Equatable) {
+        let imagePickingTask = Task {
+            if let selectedImage: UIImage = await selectImageFromGallery() {
+                do {
+                    
+                    let imagePath = try storeImageToDocuments(image: selectedImage, forKey: "PocRiImage")
+                    
+                    if let surveyId = self.surveyId {
+                        callStage(
+                            withSurveyId: surveyId,
+                            withImagePath: imagePath)
+                    } else {
+                        print("Não foi possivel ler valor de surveyId: \(surveyId)")
+                    }
+                    
+                } catch (let err) {
+                    print("Não foi possivel salvar a imagem \(err)")
+                }
+                
+            } else {
+                print("Não foi possivel selecionar imagem da galeria")
             }
         }
     }
+    
+    private func callStage() {
+        self.callStage(
+            withSurveyId: "PocriCouldNotGetSurveyId",
+            withImagePath: "UrlToTheDummy")
+    }
+    
+    private func callStage(withSurveyId surveyId: String, withImagePath urlToImage: String) {
+        
+        let deepLinkUrlString = "stage://deeplink?surveyId=\(surveyId)&dummyImageLink=\(urlToImage)"
+        
+        if let deepLinkURL = URL(string: deepLinkUrlString) {
+                        UIApplication.shared.open(deepLinkURL, options: [:], completionHandler: nil)
+                    }
+    }
+    
+    private func handleDeepLinkCall(using url: URL) {
+        if let urlComponents = URLComponents(url: url, resolvingAgainstBaseURL: false),
+           let params = urlComponents.queryItems {
+            surveyId = params.first(where: { $0.name == "surveyId" })?.value
+            param2 = params.first(where: { $0.name == "param2" })?.value
+        }
+    }
+    
+    private func selectImageFromGallery() async -> UIImage? {
+        let selectImageTask = Task {
+            print("Selecionando imagem da galeria")
+            if let data = try? await pickedItemFromGallery?.loadTransferable(type: Data.self) {
+                return UIImage(data: data)
+            }
+            
+            return nil
+        }
+        
+        return await selectImageTask.value
+    }
+    
+    private func filePathForPngExtension(forKey key: String) -> URL? {
+        let fileManager = FileManager.default
+        guard let documentURL = fileManager.urls(for: .documentDirectory,
+                                                in: FileManager.SearchPathDomainMask.userDomainMask).first else { return nil }
+        
+        return documentURL.appendingPathComponent(key + ".png")
+    }
+    
+    // Returns String with file path if succeds on saving, throws if error
+    private func storeImageToDocuments(image: UIImage,
+                                       forKey key: String) throws -> String {
+        guard let pngRepresentation = image.pngData() else {
+            throw ImageStoringErrors.couldNotParseAsData
+        }
+        guard let filePath = filePathForPngExtension(forKey: key) else {
+            throw ImageStoringErrors.couldNotGeneratePath
+        }
+        
+        do  {
+            try pngRepresentation.write(to: filePath,
+                                        options: .atomic)
+            return filePath.description
+        } catch let err {
+            print("Saving file resulted in error: ", err)
+            throw err
+        }
+    }
+}
+
+enum ImageStoringErrors: Error {
+    case couldNotParseAsData
+    case couldNotGeneratePath
 }
 
 //struct ContentView_Previews: PreviewProvider {
